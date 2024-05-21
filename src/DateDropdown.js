d71cd266f29509766ea7c8a2c1c1147d94eec4e1
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2024-05-21 22:28:21 +0000

    made minor changes to draft 1

--- src/DateDropdown.js
+++ src/DateDropdown.js
@@ -11,9 +11,7 @@ export default function DateDropdown({ dateSelected }) {
 			<Dropdown.Menu>
 				<Dropdown.Item onSelect={() => dateSelected("march15")}>March 15</Dropdown.Item>
 				<Dropdown.Item onSelect={() => dateSelected("march22")}>March 22</Dropdown.Item>
-				<Dropdown.Item onSelect={() => dateSelected("march29")}>March 29</Dropdown.Item>
-				<Dropdown.Item onSelect={() => dateSelected("april05")}>April 05</Dropdown.Item>
-				<Dropdown.Item onSelect={() => dateSelected("april15")}>April 15</Dropdown.Item>
+				
 			</Dropdown.Menu>
 		</Dropdown>
 	)
